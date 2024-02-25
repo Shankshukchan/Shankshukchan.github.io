0c43e43b52bde03ed2bd40b43dad8de3d47c9e11
Shankar dayal shukla <105650099+Shankshukchan@users.noreply.github.com>
2024-02-25 22:13:21 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,12 @@ import './index.css'
   /* The following line can be included in your src/index.js or App.js file */
 }
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+export default defineConfig({
+  plugins: [react()],
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
